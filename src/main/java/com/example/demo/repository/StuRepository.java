beb8fb014f080a8a4941ccbb64b9c8949fab5660
bala-amypo <bala@amypo.in>
2025-12-18 04:11:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/StuRepository.java
+++ src/main/java/com/example/demo/repository/StuRepository.java
@@ -1,9 +1,9 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Stuentity;
-import org.springframework.stereotype.Repository;
+// package com.example.demo.repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.entity.Stuentity;
+// import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StuRepository extends JpaRepository<Stuentity,Long>{
+// @Repository
+// public interface StuRepository extends JpaRepository<Stuentity,Long>{
 
-}
+// }
--- src/main/java/com/example/demo/serviceimpl/StudentServiceimple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceimple.java
@@ -1,21 +1,21 @@
-package com.example.demo.serviceimpl;
-import com.example.demo.service.stereotype.Service;
-import com.example.demo.service.StudentService;
-import com.example.demo.repository.StuRepository;
-import com.example.demo.entity;
-import org.springframework.beans.factory.annotation.Autowired;
+// package com.example.demo.serviceimpl;
+// import com.example.demo.service.stereotype.Service;
+// import com.example.demo.service.StudentService;
+// import com.example.demo.repository.StuRepository;
+// import com.example.demo.entity;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-@Service
-public class StudentServiceimple implements StudentService{
+// @Service
+// public class StudentServiceimple implements StudentService{
 
-//  private final StuRepository studentrepository;
-//  public StudentServiceimple(StuRepository studentrepository){
-//     this.studentrepository=studentrepository;
-//  }
- @Autowired
- StuRepository studentrepository;
+// //  private final StuRepository studentrepository;
+// //  public StudentServiceimple(StuRepository studentrepository){
+// //     this.studentrepository=studentrepository;
+// //  }
+//  @Autowired
+//  StuRepository studentrepository;
 
-public Stuentity saveStudent(Stuentity student){
-return studentrepository.save(student);
-}
-}
+// public Stuentity saveStudent(Stuentity student){
+// return studentrepository.save(student);
+// }
+// }
